6af048e46f48d363f54a2d15bab34cee696582c0
baymax-codecare <david.lee4015@gmail.com>
2022-09-20 11:52:43 -0400

    chore: update firebase credentials

--- src/chrome/credentials.ts
+++ src/chrome/credentials.ts
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-  "apiKey": "AIzaSyBw502t0eGMhhQ-ETUlOMPTju_FvqVBEz0",
-  "authDomain": "parrot-sniper.firebaseapp.com",
-  "projectId": "parrot-sniper",
-  "storageBucket": "parrot-sniper.appspot.com",
-  "messagingSenderId": "1822371412",
-  "appId": "1:1822371412:web:43697567e5a36d115607c1",
-  "measurementId": "G-PHGGL7YWBJ"
+  apiKey: "AIzaSyD20eN6zC2S_hpAxCGbN2G6VIbbwCWvJDs",
+  authDomain: "parrot-sniper-6b789.firebaseapp.com",
+  projectId: "parrot-sniper-6b789",
+  storageBucket: "parrot-sniper-6b789.appspot.com",
+  messagingSenderId: "991267212325",
+  appId: "1:991267212325:web:ac03cc408301dbef1e83cc",
+  measurementId: "G-K3JQMJ7VJ3"
 }
 
-export const vapidKey = `BPh1AC0kWb6zQ7i0eVlCnrj1Adzq-mFPtY4YEAcFhncgqwA-Q5KSkyIX6gy6tgQXjb-POrt6KNm6TXTR9HrfSFo`
+export const vapidKey = `BN6Ffs7kEXq7AsmoGAlV43HhEI85BVFtXRB9slTg-YZZDDVF6tO1nR-Y0nTRqQLCnhdzq9FeZAhQx6s_ze0NGtw`
